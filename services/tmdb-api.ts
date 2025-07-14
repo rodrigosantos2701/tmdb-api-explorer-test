a3c3341c4b7b345fbf8f237f6948e9b2ca65ceda
Rodrigo.Santos <rodrigo.santos-ext@dalenys.com>
2025-07-14 11:23:17 -0300

    fix: typo

--- services/tmdb-api.ts
+++ services/tmdb-api.ts
@@ -3,7 +3,7 @@ import axios from "axios";
 const api = axios.create({
   baseURL: "https://api.themoviedb.org/3/",
   headers: {
-    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_TOKEN}`,
+    Authorization: `Bearer ${process.env.NEXT_PUBLIC_TMDB_API_TOKEN}`,
   },
 });
 
